8ef793f10d23d314a231048fb3f33d42f8015c69
validom <34887007+validom@users.noreply.github.com>
2024-12-05 20:42:51 +0100

    Add files via upload
    from /usr/scripts/

---
+++ tuneroff.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#*******************************************
+# hier Script zum Ausschalten hinterlegen
+#*******************************************
+PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
+/usr/script/steckdose2_off.sh
---
+++ tuneron.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#*******************************************
+# hier Script zum Einschalten hinterlegen
+#*******************************************
+PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
+/usr/script/steckdose2_on.sh
